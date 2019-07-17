2fd9f61008c7f5a6acf53a48f446b9034b387d48
Catalin-Cristian Cistelican <dcsphotocluj@gmail.com>
2019-07-17 15:21:44 +0300

    Update Main.cs

--- ReqM_Tool/ReqMTool/Main.cs
+++ ReqM_Tool/ReqMTool/Main.cs
@@ -454,6 +454,80 @@ namespace ReqM_Tool
             }
            
         }
+
+        private void SaveAsExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            //Convert the XML into Dataset
+            ds.ReadXml(XmlFilePath);
+
+            //Retrieve the table fron Dataset
+            DataTable dt = ds.Tables[1];
+
+            // Create an Excel object
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            //Create workbook object
+
+
+            Microsoft.Office.Interop.Excel.Workbook workbook = excel.Workbooks.Open(Filename : XmlFilePath);
+
+            //Create worksheet object
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            // Column Headings
+            int iColumn = 0;
+
+            foreach (DataColumn c in dt.Columns)
+            {
+                iColumn++;
+                excel.Cells[1, iColumn] = c.ColumnName;
+            }
+
+            // Row Data
+            int iRow = worksheet.UsedRange.Rows.Count - 1;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                iRow++;
+
+                // Row's Cell Data
+                iColumn = 0;
+                foreach (DataColumn c in dt.Columns)
+                {
+                    iColumn++;
+                    excel.Cells[iRow + 1, iColumn] = dr[c.ColumnName];
+                }
+            }
+
+                    ((Microsoft.Office.Interop.Excel._Worksheet)worksheet).Activate();
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel|*.xlsx";
+            /* open File dialog where to save the file */
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Save the workbook
+                    workbook.SaveAs(sfd.FileName);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+
+
+            //Close the Workbook
+            workbook.Close();
+
+            // Finally Quit the Application
+            ((Microsoft.Office.Interop.Excel._Application)excel).Quit();
+        }
     }
 
     /* *******************/
